eb9eb4ed190fe2c1bc0ca88278ec7d6a807c0274
Sam Greaves <sam.greaves@meyertech.co.uk>
2018-05-11 16:10:40 +0100

    Add base model and meal card model

--- FoodCalender/FoodCalender/UserControl/FoodCalender.xaml.cs
+++ FoodCalender/FoodCalender/UserControl/FoodCalender.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FoodCalender.Model;
 
 namespace FoodCalender.UserControl
 {
@@ -20,9 +22,19 @@ namespace FoodCalender.UserControl
     /// </summary>
     public partial class FoodCalender : System.Windows.Controls.UserControl
     {
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MealCardsProperty =
+            DependencyProperty.Register("MealCards", typeof(ObservableCollection<MealCardModel>), typeof(FoodCalender), new PropertyMetadata(null));
+        
         public FoodCalender()
         {
             InitializeComponent();
+        }
+
+        public ObservableCollection<MealCardModel> MealCards
+        {
+            get { return (ObservableCollection<MealCardModel>)GetValue(MealCardsProperty); }
+            set { SetValue(MealCardsProperty, value); }
         }
     }
 }
