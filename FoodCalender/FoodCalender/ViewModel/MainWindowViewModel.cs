88a5ecbef5d0c54b8d17a2299cfe5231be2ef80c
Sam <samgreaves0@gmail.com>
2018-05-12 18:49:12 +0100

    Display food eaten

--- FoodCalender/FoodCalender/ViewModel/MainWindowViewModel.cs
+++ FoodCalender/FoodCalender/ViewModel/MainWindowViewModel.cs
@@ -1,10 +1,36 @@
-﻿namespace FoodCalender.ViewModel
+﻿using FoodCalender.Model;
+using System.Collections.ObjectModel;
+
+namespace FoodCalender.ViewModel
 {
     public class MainWindowViewModel
     {
+        private ObservableCollection<MealCardModel> _mealCards;
+
         public MainWindowViewModel()
         {
+            MealCards = new ObservableCollection<MealCardModel>();
+
+            AddMeals();
+        }
+
+        public void AddMeals()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                MealCards.Add(new MealCardModel() { Name = "N/A" });
+            }
+
+            MealCards.Add(new MealCardModel() { Name = "2 Eggs, spinich, tomato, avacado" });
+            MealCards.Add(new MealCardModel() { Name = "6 Brazil Nuts" });
 
         }
+
+        public ObservableCollection<MealCardModel> MealCards
+        {
+            get { return _mealCards; }
+            set { _mealCards = value; }
+        }
     }
+    
 }
